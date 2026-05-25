0c073b728ad1b1930a53412672b1dd84b9b27096
Pragati Dike <pragati@mail.hybrowlabs.com>
2026-05-25 13:16:58 +0530

    |nmtg|grouping of model according to the product group|

--- nmtg/public/js/item_group.js
+++ nmtg/public/js/item_group.js
@@ -43,5 +43,21 @@ frappe.ui.form.on("Item Group", {
         else if (frm.doc.custom_level === "Level 3") {
             frm.set_value("custom_item_group_title", "Sub-Product Group");
         }
+
+        set_model_query(frm);
     },
+     refresh(frm) {
+        set_model_query(frm);
+    },
+
+    
 });
+function set_model_query(frm) {
+    frm.set_query("model", "custom_model", function() {
+        return {
+            filters: {
+                "product_group": frm.doc.parent_item_group
+            }
+        };
+    });
+}
